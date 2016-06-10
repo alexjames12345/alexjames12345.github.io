071c020400ec8754ef4e85412548ef1966f86916
alexjames12345 <210540@hkis.edu.hk>
2016-06-10 10:31:52 +0800

    add game

---
+++ game/gamedev.js
@@ -0,0 +1,49 @@
+$( document ).ready(function() {
+$("#yankee-stadium").hide();
+  $("#fenway-park").hide();
+  $("#camden-yards").hide();
+  $("#rogers-centre").hide();
+  $("#comerica-park").hide();
+  $("#the-coliseum").hide();
+  $("#kauffman-stadium").hide();
+  $("#angel-stadium").hide();
+  $("#citi-field").hide();
+  $("#rogers-centre").hide();
+  $("#marlins-park").hide();
+  $("#target-field").hide();
+  $("#minute-maid-park").hide();
+  $("#safeco-field").hide();
+  $("#rangers-ballpark").hide();
+  $("#camden-yards").hide();
+  $("#rogers-centre").hide();
+  $("#the-coliseum").hide();
+  $("#us-cellular-field").hide();
+  $("#busch-stadium").hide();
+  $("#turner-field").hide();
+  $("#progressive-field").hide();
+  $("#citizens-bank-park").hide();
+  $("#pnc-park").hide();
+  $("#great-american-ballpark").hide();
+  $("#miller-park").hide();
+  $("#nationals-park").hide();
+  $("#wrigley-field").hide();
+  $("#start").show();
+  $("#hr1").hide();
+
+
+
+
+
+$("#swing1").click(function(){
+	$("#hr1").show();
+
+//this is where actions go
+});//do not delete
+ 	 
+
+
+});//doc.ready, dont delete
+
+
+
+
